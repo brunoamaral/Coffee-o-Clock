4f612272f4f143f3f394d59cde0feda675e8ccfa
Bruno Amaral <mail@brunoamaral.eu>
2016-05-15 18:25:36 +0100

    first working version

---
+++ clock.js
@@ -0,0 +1,14 @@
+function updateImage() {
+
+  var d = new Date();
+  var h = d.getHours().toString();
+  var m = d.getMinutes().toString();
+
+  image = h + m + '.JPG';
+
+  element = document.getElementById('coffeeclock');
+  element.src = 'assets/image/clock/' + image;
+}
+
+updateImage(); // call the first time
+setInterval(updateImage, 1 * 1000); // update each 1 second
