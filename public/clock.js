c6d431953d9380fd3dc5c5b3001a548da929cacf
Bruno Amaral <mail@brunoamaral.eu>
2016-06-11 13:31:46 +0100

    cleaned up code

--- public/clock.js
+++ public/clock.js
@@ -2,15 +2,17 @@ function updateImage() {
 
 	var debug = false;
 
+	var main_url = 'http://labs.brunoamaral.eu/coffeeclock/public/assets/images/clock/';
+
 	if (debug === true) {
-		var image_url = 'http://labs.brunoamaral.eu/coffeeclock/public/assets/images/clock/0100.JPG';
+		var image_url = main_url + '0100.JPG';
 	} else {
 		var d = new Date();
 		var h = d.getHours().toString();
 		var m = d.getMinutes().toString();
 			if (m < 10) { m = '0' + m};
 
-		var image_url = 'http://labs.brunoamaral.eu/coffeeclock/public/assets/images/clock/' + h + '00.JPG';
+		var image_url = main_url + h + '00.JPG';
 
 	};
 
@@ -29,5 +31,5 @@ function updateImage() {
 
 $(document).ready(function(){
 	updateImage() // call the first time
-	setInterval(updateImage, 3 * 1000) // update each 3 seconds
+	setInterval(updateImage, 15 * 1000) // update each 15 seconds
 });
